b5c4e3cdc7c837384510c7e7db7f7fabb04d7964
FroehnerG <froehner@umich.edu>
2024-11-30 18:40:30 -0500

    Finished main gameplay for final boss

--- Assets/MoveForward.cs
+++ Assets/MoveForward.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 5f; // Speed at which the object moves forward.
+    [SerializeField] float wait_time = 10f;
+    private bool canMove = false; // Flag to control movement.
+
+    private void Start()
+    {
+        StartCoroutine(WaitToStart(wait_time));
+    }
 
     void Update()
     {
-        transform.position += Vector3.forward * speed * Time.deltaTime;
+        if (canMove)
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+
+    IEnumerator WaitToStart(float wait_time)
+    {
+        yield return new WaitForSeconds(wait_time);
+        canMove = true;
     }
 }
